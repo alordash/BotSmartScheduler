4b9ca84d9aa953a98ce61230eaf448808ba5ccdb
alordash <54469554+alordash@users.noreply.github.com>
2020-09-04 19:20:40 +0300

    Moved formatting functions to formatting.js

--- BotCode/frontend/bot/botActions.js
+++ BotCode/frontend/bot/botActions.js
@@ -5,6 +5,7 @@ const rp = require('../replies/replies');
 const { dbManagement, Schedule, User } = require('../../backend/dataBase/db');
 const { parseString } = require('@alordash/parse-word-to-number');
 const { parseDate, ParsedDate, TimeList } = require('@alordash/date-parser');
+const { FormStringFormatSchedule } = require('../replies/formatting');
 
 let pendingSchedules = [];
 
@@ -148,58 +149,6 @@ function ProcessParsedDate(parsedDate, tz) {
       period_time,
       max_date
    }
-}
-
-/**@param {Date} date 
- * @returns {String} 
- */
-function FormDateStringFormat(date) {
-   let month = date.getMonth();
-   let hour = date.getHours().toString(10),
-      minute = date.getMinutes().toString(10);
-   if (hour.length <= 1) {
-      hour = '0' + hour;
-   }
-   if (minute.length <= 1) {
-      minute = '0' + minute;
-   }
-   let year = '';
-   if (date.getFullYear() != new Date().getFullYear()) {
-      year = ` ${date.getFullYear()} г.`;
-   }
-   return `${date.getDate()} ${/*constants.monthsRusRoot[month]*/'month'}${/*constants.monthsRusEnding[month][1]*/'month'} ${hour}:${minute}${year}`;
-}
-
-/**@param {ParsedDate} parsedDate 
- * @returns {String} 
- */
-function FormPeriodStringFormat(parsedDate) {
-   let result = '';
-   for(const timeType in parsedDate.period_time) {
-      if(typeof(parsedDate.period_time[timeType]) != 'undefined' && timeType != 'isOffset') {
-         result = parsedDate.period_time[timeType] + ' ' + timeType + ' ' + result;
-      }
-   }
-   return result.trim();
-}
-
-/**@param {Schedule} schedule
- * @param {ParsedDate} parsedDate 
- * @returns {String}
- */
-function FormStringFormatSchedule(schedule, parsedDate) {
-   let target_date = new Date(schedule.target_date);
-   let max_date = new Date(schedule.max_date);
-
-   let until = '';
-   let period = '';
-   if (max_date.getTime() >= Date.now()) {
-      until = '\r\nдо <b>' + FormDateStringFormat(max_date) + '</b>';
-   }
-   if (!TimeListIsEmpty(parsedDate.period_time)) {
-      period = '\r\nкаждые <b>' + FormPeriodStringFormat(parsedDate) + '</b>';
-   }
-   return `"${schedule.text}" <b>${FormDateStringFormat(target_date)}</b>${until}${period}`;
 }
 
 function GetDeletingIDsIndex(chatID, deletingIDs) {
---
+++ BotCode/frontend/replies/formatting.js
@@ -0,0 +1,60 @@
+const { ParsedDate } = require('@alordash/date-parser');
+const { Schedule } = require('../../backend/dataBase/db');
+
+/**@param {Date} date 
+ * @returns {String} 
+ */
+function FormDateStringFormat(date) {
+   let month = date.getMonth();
+   let hour = date.getHours().toString(10),
+      minute = date.getMinutes().toString(10);
+   if (hour.length <= 1) {
+      hour = '0' + hour;
+   }
+   if (minute.length <= 1) {
+      minute = '0' + minute;
+   }
+   let year = '';
+   if (date.getFullYear() != new Date().getFullYear()) {
+      year = ` ${date.getFullYear()} г.`;
+   }
+   return `${date.getDate()} ${/*constants.monthsRusRoot[month]*/'month'}${/*constants.monthsRusEnding[month][1]*/'month'} ${hour}:${minute}${year}`;
+}
+
+/**@param {ParsedDate} parsedDate 
+ * @returns {String} 
+ */
+function FormPeriodStringFormat(parsedDate) {
+    let result = '';
+    for (const timeType in parsedDate.period_time) {
+        if (typeof (parsedDate.period_time[timeType]) != 'undefined' && timeType != 'isOffset') {
+            result = parsedDate.period_time[timeType] + ' ' + timeType + ' ' + result;
+        }
+    }
+    return result.trim();
+}
+
+/**@param {Schedule} schedule
+ * @param {ParsedDate} parsedDate 
+ * @returns {String}
+ */
+function FormStringFormatSchedule(schedule, parsedDate) {
+    let target_date = new Date(schedule.target_date);
+    let max_date = new Date(schedule.max_date);
+
+    let until = '';
+    let period = '';
+    if (max_date.getTime() >= Date.now()) {
+        until = '\r\nдо <b>' + FormDateStringFormat(max_date) + '</b>';
+    }
+    if (!TimeListIsEmpty(parsedDate.period_time)) {
+        period = '\r\nкаждые <b>' + FormPeriodStringFormat(parsedDate) + '</b>';
+    }
+    return `"${schedule.text}" <b>${FormDateStringFormat(target_date)}</b>${until}${period}`;
+}
+
+exports = {
+    FormDateStringFormat,
+    FormPeriodStringFormat,
+    FormStringFormatSchedule
+}
