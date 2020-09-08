6f7ed5433dae0f5e1a2c42986c4fbc799cee495a
alordash <54469554+alordash@users.noreply.github.com>
2020-09-08 18:00:02 +0300

    Added support for only "to X time" case, fixed hours increasing

--- BotCode/backend/timeProcessing.js
+++ BotCode/backend/timeProcessing.js
@@ -45,9 +45,24 @@ function CopyTimeList(source, destination) {
  * @param {TimeList} destination 
  * @returns {TimeList} 
  */
+function SetTimeList(source, destination) {
+   for (const timeProperty in source) {
+      if (isTimeType(timeProperty)
+         && typeof (source[timeProperty]) == 'number') {
+         destination[timeProperty] = source[timeProperty];
+      }
+   }
+   return destination;
+}
+
+/**@param {TimeList} source 
+ * @param {TimeList} destination 
+ * @returns {TimeList} 
+ */
 function AddTimeList(source, destination) {
    for (const timeProperty in source) {
-      if (typeof (source[timeProperty]) == 'number') {
+      if (isTimeType(timeProperty)
+         && typeof (source[timeProperty]) == 'number') {
          destination[timeProperty] += source[timeProperty];
       }
    }
@@ -63,25 +78,26 @@ function UpdateTime(timeList, timeListDate) {
    const now = new Date();
    const tsNow = now.getTime().div(1000);
    if (timeListDate < tsNow) {
-      if (timeList.hours <= 12 && !timeList.isFixed) {
+      if (timeListDate + 12 * 3600 > tsNow && timeList.hours <= 12 && !timeList.isFixed) {
          timeList.hours = (timeList.hours + 12) % 24;
-         timeListDate += 12 * 60 * 1000;
-      }
-      let dif = tsNow - timeListDate;
-      let difInDate = new Date(tsNow * 1000 + dif * 1000);
-      let monthDif = now.getUTCMonth() - difInDate.getUTCMonth();
-      let yearDif = now.getUTCFullYear() - difInDate.getUTCFullYear();
-      dif = dif.div(60);
-      if (dif < 60 && typeof (timeList.hours) == 'undefined') {
-         timeList.hours = now.getUTCHours() + 1;
-      } else if (dif < 1440 && typeof (timeList.dates) == 'undefined') {
-         timeList.dates = now.getUTCDate() + 1;
-      } else if (monthDif < 1 && yearDif == 0 && typeof (timeList.months) == 'undefined') {
-         timeList.months = now.getUTCMonth() + 1;
-      } else if (yearDif < 1 && typeof (timeList.years) == 'undefined') {
-         timeList.years = now.getUTCFullYear() + 1;
+         timeListDate += 12 * 3600;
       } else {
-         return undefined;
+         let dif = tsNow - timeListDate;
+         let difInDate = new Date(tsNow * 1000 + dif * 1000);
+         let monthDif = now.getUTCMonth() - difInDate.getUTCMonth();
+         let yearDif = now.getUTCFullYear() - difInDate.getUTCFullYear();
+         dif = dif.div(60);
+         if (dif < 60 && typeof (timeList.hours) == 'undefined') {
+            timeList.hours = now.getUTCHours() + 1;
+         } else if (dif < 1440 && typeof (timeList.dates) == 'undefined') {
+            timeList.dates = now.getUTCDate() + 1;
+         } else if (monthDif < 1 && yearDif == 0 && typeof (timeList.months) == 'undefined') {
+            timeList.months = now.getUTCMonth() + 1;
+         } else if (yearDif < 1 && typeof (timeList.years) == 'undefined') {
+            timeList.years = now.getUTCFullYear() + 1;
+         } else {
+            return undefined;
+         }
       }
    }
    return timeList;
@@ -106,9 +122,19 @@ function ProcessParsedDate(parsedDate, tz) {
       parsedDate.target_date = AddTimeList(parsedDate.period_time, parsedDate.target_date);
       parsedDate.target_date.isOffset = true;
       target_date += period_time;
+   } else if (TimeListIsEmpty(parsedDate.target_date) && !TimeListIsEmpty(parsedDate.max_date)) {
+      parsedDate.target_date = SetTimeList(parsedDate.max_date, parsedDate.target_date);
+      target_date = dateValues.target_date.getTime().div(1000);
    }
    if (!parsedDate.target_date.isOffset) {
       console.log(`target_date is not offset, target_date :>> ${target_date}, will be: ${target_date - tz}, tz: ${tz}`);
+      let curTL = TimeListFromDate(new TimeList(), dateValues.target_date);
+      if (typeof (parsedDate.target_date.hours) == 'undefined') {
+         parsedDate.target_date.hours = curTL.hours;
+      }
+      if (typeof (parsedDate.target_date.minutes) == 'undefined') {
+         parsedDate.target_date.minutes = curTL.minutes;
+      }
       parsedDate.target_date.hours -= hours;
       parsedDate.target_date.minutes -= minutes;
       target_date -= tz;
