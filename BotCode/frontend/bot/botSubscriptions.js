31ba2d1fedcc9833ac88af61f556ebafeeffa75f
alordash <54469554+alordash@users.noreply.github.com>
2020-12-08 14:44:00 +0300

    Fixed path for replies folder

--- BotCode/frontend/bot/botSubscriptions.js
+++ BotCode/frontend/bot/botSubscriptions.js
@@ -69,7 +69,8 @@ exports.InitActions = function (bot, db) {
       }
    });
    console.log('__dirname :>> ', __dirname);
-   let repliesFiles = fs.readdirSync(path.join(__dirname, '..', 'replies'));
+//   let repliesFiles = fs.readdirSync(path.join(__dirname, '..', 'replies'));
+   let repliesFiles = fs.readdirSync(__dirname.substring(0, __dirname.lastIndexOf('/')) + '/replies');
    console.log('repliesFiles :>> ', repliesFiles);
    for (filename of repliesFiles) {
       if (path.extname(filename) == '.json') {
