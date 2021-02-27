eccddecca241f1f8d442803d29b456281ea91db5
alordash <54469554+alordash@users.noreply.github.com>
2021-02-28 00:55:44 +0300

    Renamed replies

--- BotCode/frontend/replies/replies.js
+++ BotCode/frontend/replies/replies.js
@@ -154,7 +154,7 @@ function CancelButton(language) {
  */
 function ChangedBoard(language, board, found) {
     const replies = LoadReplies(language);
-    let start = `${replies.trelloChangedBoard} ${found ? replies.trelloRemovedBoard : replies.trelloAddedBoard} ${replies.trelloBoard}`;
+    let start = `${replies.trelloChangedBoard} ${found ? replies.trelloUnpinnedBoard : replies.trelloPinnedBoard} ${replies.trelloBoard}`;
     return `${start} "<a href="${board.shortUrl}">${board.name}</a>"`;
 }
 
