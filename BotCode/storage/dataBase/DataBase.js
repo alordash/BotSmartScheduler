4d5dc9f3522d09f7618b1823bfc0d58218a3bbe5
alordash <54469554+alordash@users.noreply.github.com>
2021-04-30 16:03:22 +0000

    Made DataBase abstract, moved connections stuff to Connector.js

--- BotCode/storage/dataBase/DataBase.js
+++ BotCode/storage/dataBase/DataBase.js
@@ -1,69 +1,20 @@
-const { Pool } = require('pg');
+const { ConnectorOptions, Connector } = require('./Connector');
 const Chat = require('./classes/Chat');
 const Schedule = require('./classes/Schedule');
 const User = require('./classes/User');
 const Migrations = require('./Migrations');
 
-class DataBaseOptions {
-   /**@type {String} */
-   user;
-   /**@type {String} */
-   host;
-   /**@type {String} */
-   dataBasePath;
-   /**@type {String} */
-   password;
-   /**@type {Number} */
-   port;
-   /**@type {Object} */
-   ssl;
-}
-
 class DataBase {
-   /**@type {Pool} */
-   pool;
-   /**@type {Boolean} */
-   sending;
-   
+   constructor() {
+      if (this.constructor == DataBase) {
+         throw new Error("DataBase class can't be instantiated.");
+      }
+   }
+
    static Chats = Chat;
    static Schedules = Schedule;
    static Users = User;
 
-   constructor(options) {
-      this.pool = new Pool(options);
-      this.sending = false;
-   }
-
-   async Query(query) {
-      const client = await this.pool.connect();
-      let res;
-      try {
-         res = await client.query(query);
-      } catch (err) {
-         console.error(err.stack);
-      } finally {
-         client.release();
-         return res;
-      }
-   }
-
-   async paramQuery(query, values) {
-      const client = await this.pool.connect();
-      let res;
-      try {
-         res = await client.query({
-            rowMode: 'array',
-            text: query,
-            values
-         });
-      } catch (err) {
-         console.error(err.stack);
-      } finally {
-         client.release();
-         return res;
-      }
-   }
-   
    static async InitializeDataBase() {
       await Migrations.InitializeTables();
 
@@ -79,41 +30,15 @@ class DataBase {
       console.log(`Data base initialization finished`);
    }
 
-   /**@type {DataBase} */
-   static instance;
-
-   static get instance() {
-      if (typeof (this.instance) == 'undefined') {
-         throw "Data base connection wasn't initialized before call";
-      }
-      return this.instance;
-   }
-
-   static get sending() {
-      if (typeof (this.instance) == 'undefined') {
-         throw "Data base connection wasn't initialized before call";
-      }
-      return this.instance.sending;
-   }
-
-   /**@param {Boolean} state */
-   static set sending(state) {
-      if (typeof (this.instance) == 'undefined') {
-         throw "Data base connection wasn't initialized before call";
-      }
-      this.instance.sending = state;
-   }
-
    /**
-    * @param {DataBaseOptions} options 
+    * @param {ConnectorOptions} options 
     */
    static EstablishConnection(options) {
-      this.instance = new DataBase(options);
+      Connector.Instantiate(options);
    }
 }
 
 module.exports = {
-   DataBaseOptions,
    DataBase,
    Chat,
    Schedule,
