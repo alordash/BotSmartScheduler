dcb05b977b71c62756d9f556418a2118741b0c64
alordash <54469554+alordash@users.noreply.github.com>
2021-04-30 13:56:25 +0000

    Created DataBaseConnection class

---
+++ BotCode/storage/dataBase/connection.js
@@ -0,0 +1,80 @@
+const { Pool } = require('pg');
+
+class DataBaseConnectionOptions {
+   /**@type {String} */
+   user;
+   /**@type {String} */
+   host;
+   /**@type {String} */
+   dataBasePath;
+   /**@type {String} */
+   password;
+   /**@type {Number} */
+   port;
+   /**@type {Object} */
+   ssl;
+}
+
+class DataBaseConnection {
+   /**@type {Pool} */
+   pool;
+   /**@type {Boolean} */
+   sending;
+
+   constructor(options) {
+      this.pool = new Pool(options);
+      this.sending = false;
+   }
+
+   async Query(query) {
+      const client = await this.pool.connect();
+      let res;
+      try {
+         res = await client.query(query);
+      } catch (err) {
+         console.error(err.stack);
+      } finally {
+         client.release();
+         return res;
+      }
+   }
+
+   async paramQuery(query, values) {
+      const client = await this.pool.connect();
+      let res;
+      try {
+         res = await client.query({
+            rowMode: 'array',
+            text: query,
+            values
+         });
+      } catch (err) {
+         console.error(err.stack);
+      } finally {
+         client.release();
+         return res;
+      }
+   }
+
+   /**@type {DataBaseConnection} */
+   static instance;
+
+   static get instance() {
+      if (typeof (this.instance) == 'undefined') {
+         throw "Data base connection wasn't initialized before call";
+      }
+      return this.instance;
+   }
+
+   /**
+    * @param {DataBaseOptions} options 
+    */
+   static Instantiate(options) {
+      this.instance = new DataBaseConnection(options);
+   }
+}
+
+module.exports = {
+   DataBaseConnectionOptions,
+   DataBaseConnection
+};
