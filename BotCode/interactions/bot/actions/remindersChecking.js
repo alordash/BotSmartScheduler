3b06c1ea7f97b0e8affc47bfb541f9c50febf941
alordash <alordash@mail.ru>
2021-07-02 00:28:02 +0300

    Rewrote CheckExpiredSchedules function

--- BotCode/interactions/bot/actions/remindersChecking.js
+++ BotCode/interactions/bot/actions/remindersChecking.js
@@ -10,156 +10,80 @@ const utils = require('../../processing/utilities');
 const { Connector } = require('../../../storage/dataBase/Connector');
 const { RemoveReminders } = require('../../processing/remindersOperations');
 const Format = require('../../processing/formatting');
+const kbs = require('../static/replies/keyboards');
+const { ProcessTrelloReminder } = require('./technical');
 
-/** @param {Composer} bot */
+/**@param {Composer} bot */
 async function CheckExpiredSchedules(bot) {
-   console.log('Checking expired schedules ' + new Date());
-   Connector.instance.sending = true;
-   let now = Date.now();
-   let expiredSchedules = await DataBase.Schedules.CheckActiveSchedules(now);
-   if (expiredSchedules.length > 0) {
-      let ChatIDs = [];
-      let deletingIDs = [];
-      for (let schedule of expiredSchedules) {
-         let chatID = schedule.chatid;
-         if (chatID[0] == '_') {
-            chatID = '-' + chatID.substring(1, chatID.length);
+   let pairs = await DataBase.Schedules.GetExpiredSchedules();
+   console.log(`Checking ${pairs.length} expired schedules...`);
+   const now = Date.now();
+   let removingSchedules = [];
+   for (let pair of pairs) {
+      let schedule = pair.schedule;
+      let chatID = schedule.chatid;
+      if (chatID[0] == '_') {
+         chatID = `-${chatID.substring(1)}`;
+      }
+      if (process.env.SMART_SCHEDULER_DEBUG_MODE === 'true' && chatID != process.env.SMART_SCHEDULER_ADMIN) {
+         continue;
+      }
+      const lang = pair.lang;
+      let expired = true;
+      let decrypted = false;
+      //#region Trello processing
+      let trelloProcessionResult = await ProcessTrelloReminder(schedule, chatID);
+      expired = trelloProcessionResult.expired;
+      decrypted = trelloProcessionResult.decrypted;
+      //#endregion
+      if (!expired) {
+         continue;
+      }
+      if (!decrypted) {
+         schedule.text = Decrypt(schedule.text, schedule.chatid);
+      }
+      let repeatButton = schedule.period_time > 0 ? {} : kbs.RepeatButton(lang);
+      let msgText = Format.FormReminderMessage(schedule);
+      let isBlocked = false;
+      let msg;
+      let shouldDelete = true;
+      try {
+         if (schedule.file_id != '~' && schedule.file_id != null) {
+            msg = await BotSendAttachment(bot, +chatID, msgText, schedule.file_id, repeatButton, true);
+         } else {
+            msg = await BotSendMessage(bot, +chatID, msgText, repeatButton, true);
          }
-         if(process.env.SMART_SCHEDULER_DEBUG_MODE === 'true' && chatID != process.env.SMART_SCHEDULER_ADMIN) {
-            continue;
-         }
-         let expired = true;
-         let decrypted = false;
-         if (schedule.trello_card_id != null) {
-            try {
-               let chat = await DataBase.Chats.GetChatById(chatID);
-               if (typeof (chat) != 'undefined' && chat.trello_token != null) {
-                  let trelloManager = new TrelloManager(process.env.TRELLO_TOKEN, chat.trello_token);
-                  let card = await trelloManager.GetCard(schedule.trello_card_id);
-                  if (typeof (card) != 'undefined' && typeof (card.due) != 'undefined') {
-                     let dueTime = new Date(card.due).getTime();
-                     if (now < dueTime) {
-                        expired = false;
-
-                        if (dueTime != schedule.target_date) {
-                           DataBase.Schedules.SetScheduleTargetDate(schedule.chatid, schedule.num, dueTime);
-                        }
-
-                        const cardText = card.desc;
-                        schedule.text = Decrypt(schedule.text, schedule.chatid);
-                        decrypted = true;
-                        if (cardText != schedule.text) {
-                           DataBase.Schedules.SetScheduleText(schedule.chatid, schedule.num, Encrypt(cardText, schedule.chatid));
-                        }
-                     }
-                  } else if (typeof (card) == 'undefined') {
-                     let board = await trelloManager.GetBoard(chat.trello_board_id);
-                     if (typeof (board) == 'undefined') {
-                        DataBase.Chats.ClearChatFromTrello(chat.id);
-                     }
-                  }
-               }
-
-               if (expired) {
-                  expired = now >= schedule.target_date;
-               }
-            } catch (e) {
-               console.log(e);
+      } catch (e) {
+         console.error(e);
+         isBlocked = true;
+         console.log(`The chat "${chatID}" has blocked bot`);
+      }
+      if (!isBlocked) {
+         let repeatSchedule = new Schedule(schedule.chatid, undefined, schedule.text, schedule.username, schedule.target_date + global.repeatScheduleTime, 0, 0, schedule.file_id, ScheduleStates.repeat, msg.message_id, now);
+         DataBase.Schedules.AddSchedule(repeatSchedule);
+         let isPeriodic = schedule.period_time > 0;
+         let isLimited = schedule.max_date > 0;
+         if(isPeriodic || isLimited) {
+            let extraTime = schedule.target_date + schedule.period_time;
+            if(!isPeriodic) {
+               extraTime = schedule.max_date;
             }
-         }
-         if (!expired) {
-            continue;
-         }
-         console.log(`Expired schedule = ${JSON.stringify(schedule)}`);
-         if (!ChatIDs.includes(schedule.chatid)) {
-            ChatIDs.push(schedule.chatid);
-         }
-         let mentionUser = '';
-         if (schedule.username != 'none') {
-            mentionUser = ' @' + schedule.username;
-         }
-         let language = await DataBase.Users.GetUserLanguage(+chatID);
-         const replies = LoadReplies(language);
-         let isBlocked = false;
-         try {
-            let keyboard = {};
-            if (schedule.period_time == '0') {
-               keyboard = Extra.markup((m) =>
-                  m.inlineKeyboard([
-                     m.callbackButton(replies.repeatSchedule, `repeat`)
-                  ]).oneTime()
-               );
-            }
-            let msg;
-            let remindIcon = '⏰';
-            let scheduleNum = '';
-            if (schedule.period_time > 0) {
-               remindIcon = '🔄';
-               scheduleNum = ` /${schedule.num}`
-            }
-
-            if(!decrypted) {
-               schedule.text = Decrypt(schedule.text, schedule.chatid);
-            }
-            const remindText = `${remindIcon}${scheduleNum}${mentionUser} ${schedule.text}`;
-            try {
-               if (schedule.file_id != '~' && schedule.file_id != null) {
-                  msg = await BotSendAttachment(bot, +chatID, remindText, schedule.file_id, keyboard, true);
-               } else {
-                  msg = await BotSendMessage(bot, +chatID, remindText, {
-                     ...keyboard
-                  }, true);
-               }
-               let repeatSchedule = new Schedule(schedule.chatid, undefined, schedule.text, schedule.username, schedule.target_date + global.repeatScheduleTime, 0, 0, schedule.file_id, ScheduleStates.repeat, msg.message_id, now);
-               DataBase.Schedules.AddSchedule(repeatSchedule);
-            } catch (e) {
-               console.error(e);
-               isBlocked = true;
-            }
-         } catch (e) {
-            console.error(e);
-         }
-         let shouldDelete = true;
-         if (!isBlocked) {
-            const nowSeconds = Date.now();
-            if (schedule.period_time >= 60 && schedule.max_date >= 60) {
-               if (nowSeconds < schedule.max_date) {
-                  shouldDelete = false;
-                  await DataBase.Schedules.SetScheduleTargetDate(schedule.chatid, schedule.num, schedule.target_date + schedule.period_time);
-               }
-            } else if (schedule.period_time >= 60 && schedule.max_date < 60) {
+            if(extraTime <= schedule.max_date || !isLimited) {
+               schedule.target_date = extraTime;
+               DataBase.Schedules.SetSchedule(schedule);
                shouldDelete = false;
-               await DataBase.Schedules.SetScheduleTargetDate(schedule.chatid, schedule.num, schedule.target_date + schedule.period_time);
-            } else if (schedule.period_time < 60 && schedule.max_date >= 60) {
-               if (nowSeconds < schedule.max_date) {
-                  shouldDelete = false;
-                  await DataBase.Schedules.SetScheduleTargetDate(schedule.chatid, schedule.num, schedule.max_date);
-               }
-            }
-         }
-         if (shouldDelete) {
-            let index = utils.GetDeletingIDsIndex(schedule.chatid, deletingIDs);
-            if (index === false) {
-               deletingIDs.push({ s: `num = ${schedule.num} OR `, chatID: schedule.chatid });
-            } else {
-               deletingIDs[index].s += `num = ${schedule.num} OR `;
             }
          }
       }
-      console.log('CHECKED, removing and reordering');
-      for (let chatID of ChatIDs) {
-         let index = utils.GetDeletingIDsIndex(chatID, deletingIDs);
-         if (index !== false) {
-            let s = deletingIDs[index].s;
-            s = s.substring(0, s.length - 4);
-            await DataBase.Schedules.RemoveSchedulesQuery(chatID, s);
-         }
-         await DataBase.Schedules.ReorderSchedules(chatID);
+      if(shouldDelete || isBlocked) {
+         removingSchedules.push(schedule);
       }
-      console.log('Removed and reordered.');
    }
-   Connector.instance.sending = false;
-   console.log(`Done checking expired schedules`);
+   console.log('Done checking schedules. Removing them...');
+   await DataBase.Schedules.RemoveSchedules(removingSchedules);
+   console.log('Done removing schedules. Reordering them...');
+   await DataBase.Schedules.ReorderMultipleSchedules(removingSchedules);
+   console.log('Done expired schedules procession');
 }
 
 /**@param {Composer} bot */
