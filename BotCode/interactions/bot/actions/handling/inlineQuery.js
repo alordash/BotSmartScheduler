dddb3f4d958ecd733bb12e83de27cc9e82cf2d70
alordash <alordash@mail.ru>
2021-08-20 17:32:43 +0300

    Added search by query

--- BotCode/interactions/bot/actions/handling/inlineQuery.js
+++ BotCode/interactions/bot/actions/handling/inlineQuery.js
@@ -6,13 +6,18 @@ const { FormStringFormatSchedule, ShortenString } = require("../../../processing
  * @param {*} inlineQuery 
  */
 async function HandleInlineQuery(ctx) {
-    const schedules = await DataBase.Schedules.GetSchedules(ctx.from.id, undefined, undefined, true);
-    const user = await DataBase.Users.GetUserById(ctx.from.id);
+    const query = ctx.inlineQuery.query;
     let results = [];
     let id = 0;
+
+    const schedules = await DataBase.Schedules.GetSchedules(ctx.from.id, undefined, undefined, true);
+    const user = await DataBase.Users.GetUserById(ctx.from.id);
     const tz = user.tz;
     const language = user.lang;
+
     for (let schedule of schedules) {
+        if (!schedule.text.includes(query))
+            continue;
         let title = ShortenString(schedule.text, 20, false, '');
         let message_text = await FormStringFormatSchedule(schedule, tz, language, false, false);
         let result = { type: 'article', id: id++, title, input_message_content: { message_text, parse_mode: 'html' } };
