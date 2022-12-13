66038ea98b0299a96145f5caa80e783192395dec
alordash <alordash@mail.ru>
2022-12-13 17:19:21 +0300

    Decreased keep alive frequency

--- BotCode/keepAlive.js
+++ BotCode/keepAlive.js
@@ -5,7 +5,7 @@ let keepAliveUrl = process.env.SMART_SCHEDULER_KEEP_ALIVE_URL;
 
 function startKeepAliveService() {
     console.log("Started keep alive service");
-    utils.RepeatActionsWithPeriod(60000, async function () {
+    utils.RepeatActionsWithPeriod(900000, async function () {
         console.log("Sending request to keep alive");
         https.get(keepAliveUrl, (res) => console.log(`KeepAlive response: ${res}`));
     });
